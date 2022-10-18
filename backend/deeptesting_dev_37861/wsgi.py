b5ad365fa28ddb785309038a25e0e9484c038b91
Crowdbotics <engineering@crowdbotics.com>
2022-10-18 10:36:49 +0000

    Update models

--- backend/deeptesting_dev_37861/wsgi.py
+++ backend/deeptesting_dev_37861/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deeptesting_dev_37861.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deeptesting_dev_37861.settings")
 
 application = get_wsgi_application()
